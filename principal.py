2cb8ea26157e135b9922959e6cb639aa545249c6
LisDaniela <lisdregalon@gmail.com>
2026-06-27 13:38:47 -0400

    comentario

--- principal.py
+++ principal.py
@@ -1,6 +1,8 @@
 from controlador.controlador_principal import ControladorPrincipal
 from vista.img import recursos_rc
 
+# commit
+
 if __name__ == "__main__":
     app = ControladorPrincipal() 
     app.iniciar()         
